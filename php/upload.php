37a10716e5f3921f344f59117822f128d56d6362
kloun1592 <kloun1592@yandex.ru>
2015-02-08 08:54:28 +0200

    добвлены необходимя пробелы

--- php/upload.php
+++ php/upload.php
@@ -1,8 +1,8 @@
 <?php
     if ($_FILES["photoUpload"]["size"] > 1024*3*1024)
     {
-      echo ("Размер файла превышает три мегабайта");
-      exit;
+        echo ("Размер файла превышает три мегабайта");
+        exit;
     };
     $old_name = $_FILES["photoUpload"]["name"];
     $extension = pathinfo($old_name, PATHINFO_EXTENSION);
@@ -11,10 +11,11 @@
     $_FILES["photoUpload"]["name"] = $new_name;
     if (is_uploaded_file($_FILES["photoUpload"]["tmp_name"]))
     {
-      move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "upload/".$_FILES["photoUpload"]["name"]);
-      echo ("Файл загружен");
-    } else 
+        move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "upload/".$_FILES["photoUpload"]["name"]);
+        echo ("Файл загружен");
+    } 
+    else 
     {
-       echo("Ошибка загрузки файла");
+        echo("Ошибка загрузки файла");
     }  
 ?>
