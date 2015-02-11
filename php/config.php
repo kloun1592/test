3ec97bea783e568e7160c1f6805686fbbf23bcc4
kloun1592 <kloun1592@yandex.ru>
2015-02-11 17:54:44 +0200

    Удалены лишние файлы.

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define("SITE", "http://git.ru/");
-    define("DBHOST", "localhost");
-    define("DBUSER", "root");
-    define("DBPASS", "");
-    define("DBNAME", "gallery");
-    $db = @mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME) or die("Нет подключения к БД");
-    mysqli_set_charset($db, "utf8") or die("Не установлена кодировка соединения");
-?>
--- php/textConst.php
+++
@@ -1,5 +0,0 @@
-<?php
-    const MAX_FILE_SIZE_ECHO = "Размер фото превышает три мегабайта";
-    const FILE_UPLOAD_SUCCESS = "Файл загружен";
-    const FILE_UPLOAD_FAILED = "Ошибка загрузки файла";
-?>
