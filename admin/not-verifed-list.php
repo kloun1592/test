03dd8fb76a1f6952e11985583721553a022c21e9
kloun1592 <kloun1592@yandex.ru>
2015-02-18 14:19:54 +0200

    убрано всё лишнее

--- admin/not-verifed-list.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$query = "SELECT `photoName` FROM `photos` WHERE 1";
-    $result = $mysqli->query ( $query );    
-$image = $result->fetch_assoc();
-      header("Content-type: image/jpg ");
-      echo mysql_escape_string( $image['file'] );
